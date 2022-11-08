851b0b18531c059004b50b736f5b23d07bae0922
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-08 08:53:59 -0500

    build: add security controller tests

---
+++ src/controllers/__tests__/SecurityController.spec.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import { app } from 'src/app'
+import mockAuthData from '../../mocks/auth.json'
+
+describe('GET /secure', () => {
+  test('should return 200 OK with auth', () => {
+    return request(app).get('/secure').set('Authorization', mockAuthData.jwt).expect(200)
+  })
+
+  test('should return 401 Unauthorized without auth', () => {
+    return request(app).get('/secure').expect(401)
+  })
+
+  test('should return a 401 with an invalid token', () => {
+    return request(app).get('/secure').set('Authorization', '123').expect(401)
+  })
+})
