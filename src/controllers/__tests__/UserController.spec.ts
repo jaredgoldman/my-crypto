f44f8035623ae6cc84bc6d2e6a570e8d8e4ac9c1
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-10 21:16:34 -0500

    fix: update user controller tests

--- src/controllers/__tests__/UserController.spec.ts
+++ src/controllers/__tests__/UserController.spec.ts
@@ -4,42 +4,44 @@ import mockAuthData from '../../mocks/auth.json'
 import { UserStatus } from '@prisma/client'
 import { prismaCli } from '../../config/db'
 import { v4 as uuid } from 'uuid'
+import bcrypt from 'bcrypt'
+
+const id = uuid()
+const user = {
+  email: 'test@test.com',
+  password: 'test',
+}
 
 describe('POST /user', () => {
   test('should return a created user', async () => {
-    const user = {
-      username: 'test',
-      email: 'test@test.com',
-    }
-
     return await request(app)
       .post('/user')
-      .set('Authorization', mockAuthData.jwt)
       .send(user)
       .expect(200)
       .then(res => {
-        expect(res.body.data.user.username).toBe(user.username)
+        // store user id for later use
         expect(res.body.data.user.email).toBe(user.email)
         expect(res.body.data.user.status).toBe(UserStatus.INACTIVE)
       })
   })
 
+  test('should return a 409 error if user already exists', async () => {
+    return await request(app).post('/user').send(user).expect(409)
+  })
+
   afterAll(async () => {
-    await prismaCli.user.deleteMany({
-      where: { username: 'test' },
+    await prismaCli.user.delete({
+      where: { email: user.email },
     })
   })
 })
 
 describe('GET /user/{userId}', () => {
-  const id = uuid()
-
   beforeAll(async () => {
     await prismaCli.user.create({
       data: {
         id,
-        username: 'testUser',
-        email: 'j@j.com',
+        email: user.email,
         status: UserStatus.INACTIVE,
       },
     })
@@ -50,12 +52,90 @@ describe('GET /user/{userId}', () => {
       .get(`/user/${id}`)
       .set('Authorization', mockAuthData.jwt)
       .expect(res => {
-        console.log(res.body)
         expect(res.body.data.id).toBe(id)
       })
   })
 
+  test('should return a 404 if user not found', async () => {
+    return await request(app)
+      .get(`/user/${uuid()}`)
+      .set('Authorization', mockAuthData.jwt)
+      .expect(404)
+  })
+
   afterAll(async () => {
-    await prismaCli.user.delete({ where: { id } })
+    await prismaCli.user.delete({
+      where: { email: user.email },
+    })
+  })
+})
+
+describe('POST /user/login', () => {
+  beforeAll(async () => {
+    await prismaCli.user.create({
+      data: {
+        id,
+        email: user.email,
+        status: UserStatus.INACTIVE,
+      },
+    })
+    const hashedPassword = await bcrypt.hash(user.password, 10)
+    await prismaCli.userSecret.create({
+      data: {
+        id: uuid(),
+        userId: id,
+        secret: hashedPassword,
+      },
+    })
+  })
+
+  test('should return a token', async () => {
+    return await request(app)
+      .post('/user/login')
+      .send(user)
+      .expect(res => {
+        expect(res.body.data.token).toBeTruthy()
+        expect(res.body.data.user.email).toBe(user.email)
+        expect(res.body.data.user.status).toBe(UserStatus.ACTIVE)
+      })
+  })
+
+  afterAll(async () => {
+    await prismaCli.user.delete({
+      where: { id },
+    })
+  })
+})
+
+describe('POST /user/logout', () => {
+  beforeAll(async () => {
+    await prismaCli.user.create({
+      data: {
+        id,
+        email: user.email,
+        status: UserStatus.ACTIVE,
+      },
+    })
+  })
+  test('should return a 204', async () => {
+    await request(app)
+      .post('/user/logout')
+      .set('Authorization', mockAuthData.jwt)
+      .send({
+        id,
+      })
+      .expect(204)
+
+    const loggedOutUser = await prismaCli.user.findUnique({
+      where: { id },
+    })
+
+    expect(loggedOutUser?.status).toBe(UserStatus.INACTIVE)
+  })
+
+  afterAll(async () => {
+    await prismaCli.user.delete({
+      where: { id },
+    })
   })
 })
