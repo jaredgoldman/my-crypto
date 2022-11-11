f0fa0a47ac463212035d72a2620d4185b1c47e78
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-10 21:17:33 -0500

    fix: add auth middleware with login mode

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -2,22 +2,24 @@ import env from '../config/env'
 import * as express from 'express'
 import * as jwt from 'jsonwebtoken'
 import ApiError from '../utils/ApiError'
-import { DecodedJwt } from 'src/types/api'
+import { DecodedJwt } from '../types/api'
+import { UserService } from '../services/UserService'
+import { User } from '@prisma/client'
 
 export const expressAuthentication = (
   request: express.Request,
   securityName: string,
   scopes: string[] = []
-): Promise<DecodedJwt> => {
-  if (securityName === 'jwt') {
-    const token =
-      request.body.token || request.query.token || request.headers.authorization
-    return new Promise(async (resolve, reject) => {
-      if (!token) {
-        reject(new ApiError(401, 'Unauthorized'))
-      }
-      try {
-        jwt.verify(token, env.JWT_SIGNING_SALT, (err: any, decoded: any) => {
+): Promise<DecodedJwt | { token: string; user: User }> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (securityName === 'jwt') {
+        const token =
+          request.body.token || request.query.token || request.headers['authorization']
+        if (!token) {
+          reject(new ApiError(401, 'Unauthorized'))
+        }
+        jwt.verify(token, env.JWT_SIGNING_SALT, function (err: any, decoded: any) {
           if (err) {
             reject(new ApiError(401, 'Unauthorized', true, err))
           } else {
@@ -30,10 +32,19 @@ export const expressAuthentication = (
             resolve(decoded)
           }
         })
-      } catch (err) {
-        reject(new ApiError(401, 'Unauthorized', true, JSON.stringify(err)))
       }
-    })
-  }
-  return Promise.reject(new ApiError(401, 'Unauthorized', true))
+
+      if (securityName === 'login') {
+        const userService = new UserService()
+        const email =
+          request.body.email || request.query.email || request.headers['x-email']
+        const password =
+          request.body.password || request.query.password || request.headers['x-password']
+        resolve(await userService.login(email, password))
+      }
+    } catch (error) {
+      reject(new ApiError(401, 'Unauthorized', true, JSON.stringify(error)))
+    }
+    reject(new ApiError(401, 'Unauthorized', true))
+  })
 }
