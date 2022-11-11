8f9f17ccf366d75fe4d10d602cf14c5069086aa8
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-10 21:18:12 -0500

    build: add user exchange creation

--- src/services/ExchangeService.ts
+++ src/services/ExchangeService.ts
@@ -1,8 +1,8 @@
-import { Exchange } from '@prisma/client'
-import mockExchanges from '../mocks/exchangeData.json'
+import { Exchange, UserExchange } from '@prisma/client'
 import { prismaCli } from '../config/db'
 import { v4 as uuid } from 'uuid'
 import { Paginated } from '@src/types/api'
+import bcrypt from 'bcrypt'
 
 export type ExchangeCreationParams = Pick<Exchange, 'name' | 'url' | 'image'>
 
@@ -33,5 +33,49 @@ export class ExchangeService {
         image: params.image,
       },
     })
+  }
+
+  async createUserExchange(
+    userId: string,
+    exchangeId: string,
+    apiKey: string
+  ): Promise<UserExchange> {
+    const hashedApiKey = await bcrypt.hash(apiKey, 10)
+
+    await prismaCli.userExchangeKey.create({
+      data: {
+        id: uuid(),
+        userId,
+        exchangeId,
+        key: hashedApiKey,
+      },
+    })
+
+    return await prismaCli.userExchange.create({
+      data: {
+        id: uuid(),
+        userId,
+        exchangeId,
+      },
+    })
+  }
+
+  async getUserExchanges(
+    userId: string,
+    page: number,
+    limit: number
+  ): Promise<Paginated<UserExchange & { exchange: Exchange }>> {
+    const userExchanges = await prismaCli.userExchange.findMany({
+      where: { userId },
+      skip: page ? page * limit : 0,
+      take: limit,
+      include: { exchange: true },
+    })
+
+    return {
+      data: userExchanges,
+      next: userExchanges.length === limit ? page + 1 : null,
+      previous: page > 0 ? page - 1 : null,
+    }
   }
 }
