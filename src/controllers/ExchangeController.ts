3f408bcb04feaaba5a29d023f3d8ba7d445e7a7f
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-10 21:14:44 -0500

    build: add exchange controller endpoints

--- src/controllers/ExchangeController.ts
+++ src/controllers/ExchangeController.ts
@@ -1,35 +1,46 @@
-import { Body, Controller, Get, Path, Post, Query, Route, SuccessResponse } from 'tsoa'
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Query,
+  Route,
+  Security,
+  SuccessResponse,
+} from 'tsoa'
 import { Exchange } from '@prisma/client'
 import { ExchangeService, ExchangeCreationParams } from '../services/ExchangeService'
 import { PaginatedResponse, Response, ResponseMessage } from '../types/api'
 
 @Route('exchanges')
+@Security('jwt')
 export class ExchangeController extends Controller {
   private exchangeService = new ExchangeService()
-  @SuccessResponse('200', 'OK')
   @Get()
+  @SuccessResponse('200', 'OK')
   public async getExchanges(
     @Query('page') page: number = 0,
     @Query('limit') limit: number = 10
   ): Promise<PaginatedResponse<Exchange>> {
     const data = await this.exchangeService.getAll(page, limit)
-    return { data, message: ResponseMessage.success, statusCode: 200 }
+    return { data, message: ResponseMessage.success }
   }
 
-  @SuccessResponse('200', 'OK')
   @Get('{id}')
+  @SuccessResponse('200', 'OK')
   public async getExchange(@Path('id') id: string): Promise<Response<Exchange>> {
     const data = await this.exchangeService.get(id)
-    return { data, message: ResponseMessage.success, statusCode: 200 }
+    return { data, message: ResponseMessage.success }
   }
 
-  @SuccessResponse('201', 'Created')
   @Post()
+  @SuccessResponse('201', 'Created')
   public async createExchange(
     @Body() requestBody: ExchangeCreationParams
   ): Promise<Response<Exchange>> {
     this.setStatus(201)
     const data = await this.exchangeService.create(requestBody)
-    return { data, message: ResponseMessage.success, statusCode: 201 }
+    return { data, message: ResponseMessage.success }
   }
 }
---
+++ src/controllers/__tests__/ExchangeController.spec.ts
@@ -0,0 +1,94 @@
+import request from 'supertest'
+import { prismaCli } from '../../config/db'
+import mockAuthData from '../../mocks/auth.json'
+import { app } from '../../app'
+import { v4 as uuid } from 'uuid'
+
+// describe('GET /exchange', () => {
+//   const id = uuid()
+//   const id2 = uuid()
+//   beforeAll(async () => {
+//     await prismaCli.exchange.createMany({
+//       data: [
+//         {
+//           id: id,
+//           name: 'testExchange',
+//           url: 'https://test.com',
+//           image: 'https://test.com/image.png',
+//         },
+//         {
+//           id: id2,
+//           name: 'testExchange',
+//           url: 'https://test.com',
+//           image: 'https://test.com/image.png',
+//         },
+//       ],
+//     })
+//   })
+
+//   // TODO: Will break when actual exchanges are added
+//   test('should return two exchanges', () => {
+//     return request(app)
+//       .get('/exchanges')
+//       .set('Authorization', mockAuthData.jwt)
+//       .expect(res => {
+//         expect(res.body.data.data.length).toBe(2)
+//       })
+//   })
+
+//   afterAll(async () => {
+//     await prismaCli.exchange.deleteMany({
+//       where: { id: id || id2 },
+//     })
+//   })
+// })
+
+describe('GET /exchange/:id', () => {
+  const id = uuid()
+  beforeAll(async () => {
+    await prismaCli.exchange.create({
+      data: {
+        id: id,
+        name: 'testExchange',
+        url: 'https://test.com',
+        image: 'https://test.com/image.png',
+      },
+    })
+  })
+
+  test('should return an exchange', () => {
+    return request(app)
+      .get(`/exchanges/${id}`)
+      .set('Authorization', mockAuthData.jwt)
+      .expect(res => {
+        expect(res.body.data.id).toBe(id)
+      })
+  })
+
+  afterAll(async () => {
+    await prismaCli.exchange.delete({
+      where: { id },
+    })
+  })
+})
+
+describe('POST /exchange', () => {
+  test('should create an exchange', () => {
+    return request(app)
+      .post('/exchanges')
+      .set('Authorization', mockAuthData.jwt)
+      .send({
+        name: 'testExchange',
+        url: 'https://test.com',
+        image: 'https://test.com/image.png',
+      })
+      .expect(res => {
+        expect(res.body.data.name).toBe('testExchange')
+      })
+  })
+  afterAll(async () => {
+    await prismaCli.exchange.deleteMany({
+      where: { name: 'testExchange' },
+    })
+  })
+})
