05aaa9ff6af1444d95d2670bdfd6e7625b363f24
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-11-14 20:33:58 -0500

    fix: check for exchanges before seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,22 +3,26 @@ import { Logger } from '../src/config/logger'
 import { v4 as uuid } from 'uuid'
 
 async function seed() {
-  prismaCli.exchange.createMany({
-    data: [
-      {
-        id: uuid(),
-        name: 'Binance',
-        url: 'https://www.binance.com/',
-        image: 'https://www.binance.com/resources/img/favicon.ico',
-      },
-      {
-        id: uuid(),
-        name: 'Kraken',
-        url: 'https://www.kraken.com',
-        image: 'https://www.kraken.com/logo.png',
-      },
-    ],
-  })
+  const exchanges = await prismaCli.exchange.findMany()
+  if (exchanges.length === 0) {
+    await prismaCli.exchange.createMany({
+      data: [
+        {
+          id: uuid(),
+          name: 'Binance',
+          url: 'https://www.binance.com/',
+          image: 'https://www.binance.com/resources/img/favicon.ico',
+        },
+        {
+          id: uuid(),
+          name: 'Kraken',
+          url: 'https://www.kraken.com',
+          image: 'https://www.kraken.com/logo.png',
+        },
+      ],
+    })
+  }
+  Logger.info('Database seeding complete')
 }
 
 seed()
